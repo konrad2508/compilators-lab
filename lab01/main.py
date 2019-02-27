719409ecb30af0979bfa545d291f893c57a63326
Konrad G <konrad1997@gmail.com>
2019-02-27 14:21:18 +0100

    Fix import in main

--- lab01/main.py
+++ lab01/main.py
@@ -1,6 +1,6 @@
 import sys
 import ply.lex as lex
-import scanner  # scanner.py is a file you create, (it is not an external library)
+from . import scanner  # scanner.py is a file you create, (it is not an external library)
 
 
 if __name__ == '__main__':
