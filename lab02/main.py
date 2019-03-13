6f592554f446b361eb2642aca22972db6817e1ba
Paweł Mendroch <31183971+FrozenTear7@users.noreply.github.com>
2019-03-13 13:19:52 +0100

    Add example text files Poggers :)

--- lab02/main.py
+++ lab02/main.py
@@ -1,1 +1,58 @@
-print('sup')
+import ply.yacc as yacc
+
+tokens = scanner.tokens
+
+precedence = (
+    # to fill ...
+    ("left", '+', '-'),
+    # to fill ...
+)
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')".format(p.lineno, scanner.find_tok_column(p),
+                                                                                  p.type, p.value))
+    else:
+        print("Unexpected end of input")
+
+
+def p_program(p):
+    """program : instructions_opt"""
+
+
+def p_instructions_opt_1(p):
+    """instructions_opt : instructions """
+
+
+def p_instructions_opt_2(p):
+    """instructions_opt : """
+
+
+def p_instructions_1(p):
+    """instructions : instructions instruction """
+
+
+def p_instructions_2(p):
+    """instructions : instruction """
+
+
+# to finish the grammar
+# ....
+
+
+parser = yacc.yacc()
+
+if __name__ == '__main__':
+    filename = "example1.m"
+
+    try:
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example1.m"
+        file = open(filename, "r")
+    except IOError:
+        print("Cannot open {0} file".format(filename))
+        sys.exit(0)
+
+    parser = Mparser.parser
+    text = file.read()
+    parser.parse(text, lexer=scanner.lexer)
