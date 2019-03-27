ea1d1f5a96e82e62514b1bba4d41bbfb427db39d
Konrad G <konrad1997@gmail.com>
2019-03-27 13:31:20 +0100

    Initial commit for lab03

---
+++ lab03/parser.py
@@ -0,0 +1,215 @@
+import sys
+import ply.yacc as yacc
+import scanner
+import os
+
+tokens = scanner.tokens
+
+precedence = (
+    ("nonassoc", "IF"),
+    ("nonassoc", "ELSE"),
+    ("left", '=', 'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN'),
+    ("left", '<', '>', 'LTE', 'GTE', 'NEQ', 'EQ'),
+    ("left", '+', '-'),
+    ("left", '*', '/'),
+    ("left", 'DOTADD', 'DOTSUB'),
+    ("left", 'DOTMUL', 'DOTDIV'),
+    ('nonassoc', 'TRANSPOSE')
+)
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')".format(p.lineno, scanner.find_tok_column(p),
+                                                                                  p.type, p.value))
+    else:
+        print("Unexpected end of input")
+
+
+def p_start(p):
+    """start    : operation"""
+
+
+def p_operation(p):
+    """operation    : operation operation
+                    | '{' operation '}'
+                    | simple_operation"""
+
+
+def p_simple_operation(p):
+    """simple_operation : assignment
+                        | matrix_assignment
+                        | spec_function
+                        | if_flow
+                        | for_flow
+                        | while_flow"""
+
+
+def p_assignment_op(p):
+    """assignment_op    : '='
+                        | ADDASSIGN
+                        | SUBASSIGN
+                        | MULASSIGN
+                        | DIVASSIGN"""
+
+
+def p_assignment(p):
+    """assignment   : ID assignment_op assignment_expression
+                    | ID element assignment_op expression"""
+
+
+def p_element(p):
+    """element  : '[' index ']'"""
+
+
+def p_index(p):
+    """index    : INT
+                | INT ',' index"""
+
+
+def p_assignment_expression(p):
+    """assignment_expression    : matrix_func '(' INT ')' ';'
+                                | expression"""
+
+
+def p_matrix_func(p):
+    """matrix_func  : ZEROS
+                    | EYE
+                    | ONES"""
+
+
+def p_simple_val(p):
+    """simple_val   : INT
+                    | FLOAT"""
+
+
+def p_matrix_assignment(p):
+    """matrix_assignment    : ID '=' '[' custom_matrix ']' ';'"""
+
+
+def p_custom_matrix(p):
+    """custom_matrix    : matrix_row
+                        | matrix_row additional_matrix_row"""
+
+
+def p_additional_matrix_row(p):
+    """additional_matrix_row    : additional_matrix_row additional_matrix_row
+                                | ';' matrix_row"""
+
+
+def p_matrix_row(p):
+    """matrix_row   : simple_val
+                    | simple_val additional_simple_val"""
+
+
+def p_additional_simple_val(p):
+    """additional_simple_val    : additional_simple_val additional_simple_val
+                                | ',' simple_val"""
+
+
+def p_spec_function(p):
+    """spec_function    : BREAK ';'
+                        | CONTINUE ';'
+                        | RETURN extended_val ';'
+                        | PRINT extended_val ';'"""
+
+
+def p_extended_val(p):
+    """extended_val : STRING
+                    | val"""
+
+
+def p_val(p):
+    """val  : ID
+            | ID element
+            | simple_val
+            | val ',' ID"""
+
+
+def p_if_flow(p):
+    """if_flow  : IF '(' condition ')' next_op
+                | IF '(' condition ')' next_op ELSE next_op"""
+
+
+def p_condition_val(p):
+    """condition    : simple_val '>' simple_expression
+                    | simple_val '<' simple_expression
+                    | simple_val GTE simple_expression
+                    | simple_val LTE simple_expression
+                    | simple_val NEQ simple_expression
+                    | simple_val EQ simple_expression"""
+
+
+def p_condition_id(p):
+    """condition    : ID '>' simple_expression
+                    | ID '<' simple_expression
+                    | ID GTE simple_expression
+                    | ID LTE simple_expression
+                    | ID NEQ simple_expression
+                    | ID EQ simple_expression"""
+
+
+def p_next_op(p):
+    """next_op  : simple_operation
+                | '{' operation '}'"""
+
+
+def p_while_flow(p):
+    """while_flow   : WHILE '(' condition ')' next_op"""
+
+
+def p_for_flow(p):
+    """for_flow : FOR ID '=' range next_op"""
+
+
+def p_range(p):
+    """range    : INT ':' INT
+                | INT ':' ID
+                | ID ':' ID"""
+
+
+def p_bin_op(p):
+    """bin_op   : '+'
+                | '-'
+                | '*'
+                | '/'
+                | DOTADD
+                | DOTSUB
+                | DOTMUL
+                | DOTDIV"""
+
+
+def p_un_op(p):
+    """un_op    : '-'"""
+
+
+def p_expression(p):
+    """expression   : simple_expression ';'"""
+
+
+def p_simple_expression(p):
+    """simple_expression    : simple_expression bin_op simple_expression
+                            | un_op simple_expression
+                            | simple_expression TRANSPOSE
+                            | '(' simple_expression ')'
+                            | val"""
+
+
+if __name__ == '__main__':
+    filename = "example1.m"
+
+    try:
+        file = open(filename, "r")
+    except IOError:
+        print("Cannot open {0} file".format(filename))
+        sys.exit(0)
+
+    if os.path.exists('parser.out'):
+        os.remove('parser.out')
+
+    if os.path.exists('parsetab.py'):
+        os.remove('parsetab.py')
+
+    parser = yacc.yacc()
+    text = file.read()
+    parser.parse(text, lexer=scanner.lexer)
