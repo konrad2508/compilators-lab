2844f1a5ffb8621474040148cf29036758355d2f
Konrad G <konrad1997@gmail.com>
2019-03-27 13:41:18 +0100

    Program starting

--- lab03/main.py
+++ lab03/main.py
@@ -1,19 +1,21 @@
 import sys
 import ply.yacc as yacc
-from Mparser import Mparser
+import Mparser
 from TreePrinter import TreePrinter
 
 if __name__ == '__main__':
+    filename = ""
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example1.m"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
         sys.exit(0)
 
-    Mparser = Mparser()
+    scanner = Mparser.scanner
+
     parser = yacc.yacc(module=Mparser)
     text = file.read()
-    ast = parser.parse(text, lexer=Mparser.scanner)
+    ast = parser.parse(text, lexer=scanner.lexer)
     ast.printTree()
