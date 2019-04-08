ab54eeaf43720884491faf9196d63f7e363b7045
FrozenTear7 <RainbowPablo7@gmail.com>
2019-04-08 10:57:04 +0200

    Remove ';' from vector and matrix to eliminate shift/reduce

--- lab03/Mparser.py
+++ lab03/Mparser.py
@@ -81,8 +81,8 @@ def p_assignment_operator(p):
 def p_assignment(p):
     """assignment   : element assignment_operator matrix_fun ';'
                     | element assignment_operator expression ';'
-                    | element assignment_operator vector ';'
-                    | element assignment_operator matrix ';'"""
+                    | element assignment_operator vector
+                    | element assignment_operator matrix"""
     p[0] = AST.Assign(p[1], p[2], p[3])
 
 
